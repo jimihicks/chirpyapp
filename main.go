ec23a99faa794272e031debc22f90648a0510f3b
jimihicks <jimidavish@gmail.com>
2025-08-21 21:36:11 -0700

    added endpoints to track metrics

--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middleWareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	message := fmt.Sprintf(`
+	<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+	</body>
+	</html>
+	`, cfg.fileServerHits.Load())
+	w.Write([]byte(message))
+}
 
 func main() {
 	const filepathroot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
+
 	log.Println("Starting server...")
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathroot)))
+	mux.Handle("/app/", apiCfg.middleWareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathroot)))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Handler: mux,
